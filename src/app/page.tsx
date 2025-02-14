18f9c012b990b98e8b33a2a52f0d5cc064dc2f49
DiyanshuTiwaari <sontitiwa569@gmail.com>
2025-02-14 18:09:18 +0530

    fixed folderItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Folder from './components/Folder';
 import { FolderItem } from '../types/types';
@@ -7,21 +6,21 @@ import AutoSaveTextBox from './components/AutoSaveTextBox';
 // Simulate fetching data from a JSON file
 const fetchData = async (): Promise<FolderItem> => {
   const data = await import('../../public/Data.json');
-  return data.default.root;
+  return data.default.root as FolderItem; // Type assertion to ensure correct type
 };
 
 export default async function Home() {
   const data = await fetchData();
 
-  return ( <>
-    <div>
-    <AutoSaveTextBox/>
-    </div>
-    <div className='max-w-xl mx-auto mt-6 px-4'>
-       <h1>Folder traversal system </h1>
-     
-      <Folder folder={data} />
-    </div>
+  return (
+    <>
+      <div>
+        <AutoSaveTextBox />
+      </div>
+      <div className="max-w-xl mx-auto mt-6 px-4">
+        <h1>Folder traversal system</h1>
+        <Folder folder={data} />
+      </div>
     </>
   );
 }
